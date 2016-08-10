aae7e66d049fe02dce31607d6ac94aa8cf1a0659
王凯岩_KY2 <kaiyanky2@qq.com>
2016-08-10 22:50:57 +0800

    添加模型层类Crime

---
+++ app/src/main/java/com/app/wangkaiyan/criminalintent/Crime.java
@@ -0,0 +1,28 @@
+package com.app.wangkaiyan.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by wangkaiyan on 2016/8/10.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime(UUID id) {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
